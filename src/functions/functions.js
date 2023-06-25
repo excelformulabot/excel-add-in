e624d4f8883768c9530c0001e7aededcadfb1a50
excelformulabot <115473780+excelformulabot@users.noreply.github.com>
2023-06-25 14:52:23 -0400

    Update functions.js

--- src/functions/functions.js
+++ src/functions/functions.js
@@ -3,14 +3,15 @@
 const BASE_URL = "https://formulabot.com/api/1.1/wf/";
 
 /**
- * =FORMULABOT_CLASSIFY(cell_to_classify_type, cell_to_classify, cell_to_classify_based_on, option1, option2,...)
- * Ex:=FORMULABOT_CLASSIFY("Company",A1,"Industry","Tech","Retail")
+ * =FORMULABOT.CLASSIFY(cell_to_classify_type, cell_to_classify, cell_to_classify_based_on, option1, option2,...)
+ * Ex:=FORMULABOT.CLASSIFY("Company",A1,"Industry","Tech","Retail")
  * @customfunction
  * @param {string} cell_to_classify_type Input String
  * @param {string} cell_to_classify Input String
  * @param {string} cell_to_classify_based_on Input String
  * @param {string[][][]} list_options Multiple ranges of values.
  * @returns {string} The Formulabot result
+ * @helpurl http://www.formulabot.com.com/features/classify
  */
 function CLASSIFY(cell_to_classify_type, cell_to_classify, cell_to_classify_based_on, list_options) {
   // check if user is active
@@ -42,12 +43,13 @@ function CLASSIFY(cell_to_classify_type, cell_to_classify, cell_to_classify_base
 }
 
 /**
- * =FORMULABOT_EXTRACT(cell_to_extract_info, info_to_extract)
- * Ex: =FORMULABOT_EXTRACT(F1,"company name")
+ * =FORMULABOT.EXTRACT(cell_to_extract_info, info_to_extract)
+ * Ex: =FORMULABOT.EXTRACT(F1,"company name")
  * @customfunction
  * @param {string} cell_to_extract_info Input String
  * @param {string} info_to_extract Input String
  * @returns {string} The Formulabot result
+ * @helpurl http://www.formulabot.com.com/features/extract
  */
 function EXTRACT(cell_to_extract_info, info_to_extract) {
   // check if user is active
@@ -74,12 +76,13 @@ function EXTRACT(cell_to_extract_info, info_to_extract) {
 }
 
 /**
- * =FORMULABOT_SENTIMENT(cell_to_analyze_sentiment, option1, option2,...)
- * Ex: =FORMULABOT_SENTIMENT(F1,"positive","negative","neutral")
+ * =FORMULABOT.SENTIMENT(cell_to_analyze_sentiment, option1, option2,...)
+ * Ex: =FORMULABOT.SENTIMENT(F1,"positive","negative","neutral")
  * @customfunction
  * @param {string} cell_to_analyze_sentiment Input String
  * @param {string[][][]} options Multiple ranges of values.
  * @returns {string} The Formulabot result
+ * @helpurl http://www.formulabot.com.com/features/sentiment-analysis
  */
 function SENTIMENT(cell_to_analyze_sentiment, options) {
   // check if user is active
@@ -105,12 +108,13 @@ function SENTIMENT(cell_to_analyze_sentiment, options) {
 }
 
 /**
- * =FORMULABOT_INFO(cell_to_get_info, info_request)
- * Ex: =FORMULABOT_INFO(F1,"tallest building")
+ * =FORMULABOT.INFO(cell_to_get_info, info_request)
+ * Ex: =FORMULABOT.INFO(F1,"tallest building")
  * @customfunction
  * @param {string} cell_to_get_info Input String
  * @param {string[][][]} info_request Multiple ranges of values.
  * @returns {string} The Formulabot result
+ * @helpurl http://www.formulabot.com.com/features/retrieve-information
  */
 function INFO(cell_to_get_info, info_request) {
   // check if user is active
@@ -136,11 +140,12 @@ function INFO(cell_to_get_info, info_request) {
 }
 
 /**
- * =FORMULABOT_FREEFORM(input_any_info)
- * Ex: =FORMULABOT_FREEFORM( “Write me a thank you note to ”, B10 ,“for gifting me a”, C10)
+ * =FORMULABOT.FREEFORM(input_any_info)
+ * Ex: =FORMULABOT.FREEFORM( “Write me a thank you note to ”, B10 ,“for gifting me a”, C10)
  * @customfunction
  * @param {string[][][]} input_any_info Multiple ranges of values.
  * @returns {string} The Formulabot result
+ * @helpurl http://www.formulabot.com.com/features/freeform
  */
 function FREEFORM(input_any_info) {
   // check if user is active
@@ -165,13 +170,14 @@ function FREEFORM(input_any_info) {
 }
 
 /**
- * =FORMULABOT_INFER(input_examples, output_examples, input_to_predict)
- * Ex: =FORMULABOT_INFER(A1:A5,B1:B5,A6)
+ * =FORMULABOT.INFER(input_examples, output_examples, input_to_predict)
+ * Ex: =FORMULABOT.INFER(A1:A5,B1:B5,A6)
  * @customfunction
  * @param {string} input_to_predict Input String
  * @param {string[][]} input_examples Multiple ranges of values.
  * @param {string[][]} output_examples Multiple ranges of values.
  * @returns {string} The Formulabot result
+ * @helpurl http://www.formulabot.com.com/features/inference
  */
 function INFER(input_examples, output_examples, input_to_predict) {
   // check if user is active
@@ -207,5 +213,33 @@ function INFER(input_examples, output_examples, input_to_predict) {
     return result;
   } catch (e) {
     return "No result found";
+  }
+}
+
+
+
+async function callAPI(endpoint, payload) {
+  const BASE_URL = "https://formulabot.com/api/1.1/wf/";
+  var Token = localStorage.getItem("token");
+  console.log(Token);
+  try {
+    const url = BASE_URL + endpoint;
+    const options = {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${Token}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(payload), // Use "body" instead of "payload" for the request body
+    };
+    console.log("CALL API OPTIONS", url, options);
+    var response = await fetch(url, options);
+    console.log(response);
+    var resp = await response.json();
+    console.log("CALL API RESP", resp);
+    var result = resp?.response?.output ?? "No Result Found";
+    return result;
+  } catch (e) {
+    return "No Result Found " + e;
   }
 }
